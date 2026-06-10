609e66b45d8a38b09903ef76768c64bf4ff9adce
unnisemai2 <unnisemai@gmail.com>
2026-06-10 17:49:54 -0300

    Comentarios de funcion del requerimiento 4 de consigna de trabajo practico
    se agregaron los comentarios necesarios del integrador

--- lisp/core.lisp
+++ lisp/core.lisp
@@ -1,9 +1,21 @@
+;; ========================================================
+;; FUNCIÓN: duracion-ciclo
+;; NATURALEZA: Pura (Dado la operacion, siempre retorna un numero)
+;; ESTRATEGIA: Simple
+;; IMPACTO: No destructiva
+;; ========================================================
 (defun duracion-ciclo (duracion-rojo duracion-amarillo duracion-verde)
 
     (+ duracion-rojo duracion-amarillo duracion-verde)
 
 )
 
+;; ========================================================
+;; FUNCIÓN: recomendacion
+;; NATURALEZA: Pura (Dado la operacion, siempre retorna uno de 3 valores)
+;; ESTRATEGIA: Simple
+;; IMPACTO: No destructiva
+;; ========================================================
 (defun recomendacion (duracion)
 
     (cond
