f7d82aed0f8edefde5576499cd143f5a0df25f74
gaston soto <veggitto763@gmail.com>
2026-06-10 21:50:57 -0300

    punto 5

--- lisp/core.lisp
+++ lisp/core.lisp
@@ -3,3 +3,6 @@
 (defun transicion (color-actual cambiar-a)
  (if (equal color-actual 'en-rojo)
    (list color-actual "cambiar-a-verde")))
+;requerimiento 5
+(defun ciclos-por-tiempo(minutos)
+	(/ minutos 3.6))
