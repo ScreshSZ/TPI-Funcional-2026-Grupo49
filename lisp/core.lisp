b393491e00a268a2e3e47bef297e4a219988baa0
gaston soto <veggitto763@gmail.com>
2026-06-10 21:55:10 -0300

    probe recursividad de cola

--- lisp/core.lisp
+++ lisp/core.lisp
@@ -4,5 +4,8 @@
  (if (equal color-actual 'en-rojo)
    (list color-actual "cambiar-a-verde")))
 ;requerimiento 5
-(defun ciclos-por-tiempo(minutos)
-	(/ minutos 3.6))
+ (defun ciclos-por-tiempo-v2 (minutos)
+  (let ((duracion-ciclo 3.6))
+    (if (< minutos duracion-ciclo)
+        0
+        (+ 1 (ciclos-por-tiempo-v2 (- minutos duracion-ciclo))))))
