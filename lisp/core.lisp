6ae434424752df0cf3c3eb7cc8fb96604da1989c
Miño Martin <martin1372006@gmail.com>
2026-06-21 19:29:56 -0300

    Correccion del requerimiento 7

--- lisp/core.lisp
+++ lisp/core.lisp
@@ -150,7 +150,7 @@
 (defun ejecutar-qa-completo ()
     (progn
         (format t "~%--- PRUEBAS REQ 6: DISTRIBUCION TEMPORAL ---~%")
-        (format t "Caso Normal (93s Rojo, 9s Amarillo, 123s Verde):~%")
+        (format t "Caso Normal (93s Rojo, 9s Verde, 123s Amarillo):~%")
         (print (distribucion-porcentual 93 9 123))
         
         (format t "~%Caso Error (Letras en vez de numeros):~%")
